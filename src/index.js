11150516b89ea18ed543a496662d13d67ceb6b96
Kristofer M White <me@kmwhite.net>
2017-11-05 16:59:52 -0600

    Setup base Webpack Dev Server

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/*
+ * ./src/index.js Webpack entry point
+ *
+ */
+
+console.log('[BS] Loaded')
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@
  */
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -14,5 +21,8 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
 }
